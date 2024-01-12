638f806f7c345fa51fe28b62ec44eb49758dd7ab
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2024-01-13 01:34:36 +0500

    going to implement the dispatach

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import { useSelector } from "react-redux"
 
 const App = () => {
   const counter = useSelector((state)=>state.counter);
+   
+  const increment = () =>{
+
+  }
+  const decrement = () =>{
+    
+  }
+
   return (
     <div>
 
       <h1>Counter </h1>
       <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
-  )
+  ) 
 }
 
 export default App
