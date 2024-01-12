4117cb9aa68d756a4f8272cc717a4309bc4c860a
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2024-01-13 02:53:46 +0500

    done with basic counter  app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch} from "react-redux"
 
 const App = () => {
   const counter = useSelector((state)=>state.counter);
-   
-  const increment = () =>{
+  const dispatch = useDispatch(); 
 
-  }
+
+  const increment = () =>{
+    dispatch({type:'INC'});
+  };
   const decrement = () =>{
-    
+    dispatch({type:'DEC'})
   }
+
+  const addByTen = ()=>{
+    dispatch({type:"AddByTen"});
+  }
+
 
   return (
     <div>
 
       <h1>Counter </h1>
       <h2>{counter}</h2>
-      <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={addByTen}> Add by 10 </button>
     </div>
   ) 
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,27 @@
 import { createStore } from "redux";
 
-const reducerFunction = (state = { counter: 10 },action)=>{
-    return {...state,counter:state.counter+5}
+const reducerFunction = (state = { counter: 0 },action)=>{
+
+    // return {...state,counter:state.counter+5}
+
+  //Limitations  
+        // Synchronous Function
+        // We should not mutate the original state(Should be the copy of original state)
+
+        if(action.type === "INC"){
+            return {...state,counter:state.counter + 1};
+        }
+        else if(action.type === "DEC"){
+            return {...state,counter:state.counter - 1};
+        }
+        else if(action.type === "AddByTen"){
+            return{...state,counter:state.counter+ 10}
+        }
+        else {
+            return state;
+        }
+
+
 }
 
 const store = createStore(reducerFunction);
